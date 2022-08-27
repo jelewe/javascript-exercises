fc6ae8f32c045811e90799ddee3fb9d0191bba1a
Jessie <hire.jesslw@gmail.com>
2022-08-27 08:57:13 -1000

    completed exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+ //   let repeatedString ="";
+//while (num > 0) {
+   // repeatedString += str;
+   // num--;
+//}
+ //   return repeatedString
+//}
 
-};
+//repeatString('hey',3);
+
+if (num > 0 ) {
+    return str.repeat(num)
+} else if (num == 0) {
+    return "" 
+} else {
+    return "ERROR"
+}
+}
+
+repeatString('hey',3)
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    return joinArray;
+}
 
-};
+reverseString("Hello!")
 
+// get last letter from string
+// store in variable R
+// get last -1 letter from string, store in variable R
+// get last -2 letter from string, store in variable R
+// repeat until no more letters
+// print R
+
+reverseString("Hello World")
 // Do not edit below this line
 module.exports = reverseString;
