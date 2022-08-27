a0f8b1df80d00e1b6f7e3fd2c4859dc0afe00753
Jessie <hire.jesslw@gmail.com>
2022-08-27 10:18:30 -1000

    solved first test, fails second test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray,removeVariable) {
+    let resultArray = myArray.filter(removeV);
 
+    function removeV(item) {
+        return item !== removeVariable;
+    }
+    return resultArray;
 };
+
+ //   Array.from(myArray);
+ //   myArray.pop(removeVariable);
+ //   return myArray;
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
