3893d54643325451a56bc799c4dfa4728a11a738
Jessie <hire.jesslw@gmail.com>
2022-08-28 12:38:08 -1000

    exercise 07 passes all tests for fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
 const ftoc = function(temperature) {
   let celsius = 0;
   celsius = (temperature - 32) * (5 / 9);
-  return celsius;
+  let result = celsius.toFixed(1);
+  result = Number(result);
+  return result;
 };
 
 const ctof = function() {
