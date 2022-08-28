255b5be49fd722a98e29060690d69318edb4efa4
Jessie <hire.jesslw@gmail.com>
2022-08-28 10:09:35 -1000

    exercise 06 passes all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,12 +1,22 @@
 const leapYears = function(year) {
     let leapYear = 1;
     if (year % 4 == 0) {
-        year = 1
-    } else {
-        year = 0;
+        if (year % 100 != 0) {
+            year = 1;
+         } 
+         else if (year % 400 == 0) {
+                year = 1;
+            } 
+            else {
+                year = 0;
+            }
+        }
+        else {
+            year = 0;
+    } 
+    return year === leapYear;
     }
-return year === leapYear;
-};
+
 
 // Do not edit below this line
 module.exports = leapYears;
