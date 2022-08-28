862759b56b22c2d542dd7f559e9d52c3b4c06b12
Jessie <hire.jesslw@gmail.com>
2022-08-28 09:04:44 -1000

    exercise 05 all tests pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,8 @@
 const sumAll = function(num1,num2) {
     let sum = 0;
-    if (num1 < 0 || num2 < 0) {
+    if (typeof num1 !== "number" || typeof num2 !== "number") {
+        return "ERROR";
+    } else if (num1 < 0 || num2 < 0) {
         return "ERROR";
     } else if (num1 < num2) {
         for (let i = num1; i <= num2; i++) {
