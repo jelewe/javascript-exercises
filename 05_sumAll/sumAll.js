518049207c6d8ff033dfcf88464f92721d813c80
Jessie <hire.jesslw@gmail.com>
2022-08-27 14:47:23 -1000

    passes first test, fails second test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
+let sum = 0;
 
+const sumAll = function(num1,num2) {
+    for (let i = num1; i <=num2; i++) {
+        sum += i;
+    }
+return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
