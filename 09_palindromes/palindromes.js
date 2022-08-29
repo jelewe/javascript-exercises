7dd2a557e0ac4671e735b677124be168a0872ced
Jessie <hire.jesslw@gmail.com>
2022-08-28 19:23:30 -1000

    ex 09 passes all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase().trim();
+    string = string.replaceAll("!", "");
+    string = string.replaceAll(",", "");
+    string = string.replaceAll(".", "");
+    string = string.replaceAll(" ", "");
+    let splitString = string.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    return joinArray === string;
+    
 };
 
 // Do not edit below this line
