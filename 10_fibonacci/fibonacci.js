64f747291b67ec21ef71b8b61cdde0a9a703a0c1
Jessie <hire.jesslw@gmail.com>
2022-08-30 08:43:29 -1000

    ex 10 fibonacci passes all tets

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    let a = 0;
+    let b = 1;
+    let c = num;
+    if (num < 0) {
+        return "OOPS";
+    } else {
+    for (i = 2; i <=num; i++) {
+        c = a+b;
+        a = b;
+        b = c;
+        }
+    return c;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
