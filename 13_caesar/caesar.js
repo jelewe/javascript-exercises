e13af095d1ea573b0ac6342fe26103c6dbf740cc
Jessie <hire.jesslw@gmail.com>
2022-09-05 23:27:26 -1000

    ex13 cipher works except large & negative shifts

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,4 +1,25 @@
-const caesar = function() {
+//const negShift = function(num) {
+ //   if (num > 0) {
+ //       num = num;
+ //   } else {
+  //      num += 26;
+ //   }
+ //   return num;
+//}
+
+const caesar = function(string,shift) {
+    var newString = "";
+     for (i = 0; i < string.length; i++) {
+            let ascii = string[i].charCodeAt();
+             if (ascii >= 32 && ascii <= 46) { //keeps spacing and punctuation
+                newString += String.fromCharCode(ascii);
+            } else if (ascii >= 65 && ascii <= 90) { //uppercase
+             ((ascii + shift) <= 90) ? newString +=  String.fromCharCode((ascii + shift)) : newString +=  String.fromCharCode((ascii + shift) % 90 + 64);
+         } else {
+                ((ascii + shift) <= 122 ) ? newString +=  String.fromCharCode((ascii + shift)) : newString +=  String.fromCharCode((ascii + shift) % 122 + 96);
+         }
+    }
+return newString;
 
 };
 
